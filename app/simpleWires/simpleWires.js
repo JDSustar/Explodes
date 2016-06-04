de2b1a17998ebedd1da096ef024a9e9e221f54b7
Chris Hofer <HoferC@users.noreply.github.com>
2016-06-04 17:08:53 -0400

    Renamed view2 to simpleWires

--- app/simpleWires/simpleWires.js
+++ app/simpleWires/simpleWires.js
@@ -26,16 +26,16 @@ function isLastDigitOdd(serial)
     return false;
 }
 
-angular.module('myApp.view2', ['ngRoute', 'ui.bootstrap'])
+angular.module('myApp.simpleWires', ['ngRoute', 'ui.bootstrap'])
 
     .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/view2', {
-            templateUrl: 'view2/view2.html',
-            controller: 'View2Ctrl'
+        $routeProvider.when('/simpleWires', {
+            templateUrl: 'simpleWires/simpleWires.html',
+            controller: 'SimpleWireController'
         });
     }])
 
-    .controller('View2Ctrl', ['$scope', '$route', function ($scope, $route) {
+    .controller('SimpleWireController', ['$scope', '$route', function ($scope, $route) {
         $scope.reloadRoute = function () {
             $route.reload();
         };
