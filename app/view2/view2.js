2e2231fe983e860fab6c2ee69dc824cc67d88b0a
Chris Hofer <HoferC@users.noreply.github.com>
2016-06-04 16:11:49 -0400

    Trying to count wires

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,6 +1,22 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+var colorCounts =
+{
+    Blue: 0,
+    Red: 0,
+    White: 0,
+    Yellow: 0,
+    Black: 0,
+    reset: function() {
+        this.Blue = 0;
+        this.Red = 0;
+        this.White = 0;
+        this.Yellow = 0;
+        this.Black = 0;
+    }
+};
+
+angular.module('myApp.view2', ['ngRoute', 'ui.bootstrap'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/view2', {
@@ -9,14 +25,25 @@ angular.module('myApp.view2', ['ngRoute'])
         });
     }])
 
-    .controller('View2Ctrl',  [function () {
+    .controller('View2Ctrl', ['$scope', '$route', function ($scope, $route) {
+        $scope.reloadRoute = function () {
+            $route.reload();
+        };
         var view2 = this;
+        view2.serial = "";
         view2.wires = [
-            { name:"1", color: "None" },
-            { name:"2", color: "None" },
-            { name:"3", color: "None" },
-            { name:"4", color: "None" },
-            { name:"5", color: "None" },
-            { name:"6", color: "None" }
+            {name: "1", color: "None"},
+            {name: "2", color: "None"},
+            {name: "3", color: "None"},
+            {name: "4", color: "None"},
+            {name: "5", color: "None"},
+            {name: "6", color: "None"}
         ];
+        view2.updateResults = function () {
+            colorCounts.reset();
+            for (var wire in view2.wires) {
+                colorCounts[wire.color] = colorCounts[wire.color] + 1;
+            }
+            console.log(colorCounts);
+        }
     }]);
