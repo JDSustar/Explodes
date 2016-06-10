dda8559f187b33752f7e82b9463e2487973f3af4
Jason D. Sustar <jason.sus@gmail.com>
2016-06-09 21:04:29 -0400

    WebStorm is dying a slow and painful death

--- app/memory/memory.js
+++ app/memory/memory.js
@@ -1,3 +1,44 @@
+function stage (stageNumber) {
+    this.stageNumber = stageNumber;
+    this.firstButton = null;
+    this.secondButton = null;
+    this.thirdButton = null;
+    this.fourthButton = null;
+    this.buttonNumbers = null;
+    this.buttonString = null;
+    this.screenNumber = null;
+    this.numberPressed = null;
+    this.buttonPressed = null;
+}
+
+stage.prototype.parseButtonString = function() {
+    this.firstButton = this.buttonString[0];
+    this.secondButton = this.buttonString[1];
+    this.thirdButton = this.buttonString[2];
+    this.fourthButton = this.buttonString[3];
+};
+
+stage.prototype.update = function() {
+    if(this.buttonNumbers) {
+        this.buttonString = this.buttonNumbers.toString();
+    } else {
+        return;
+    }
+
+    if(this.buttonString.length != 4) {
+        return;
+    }
+
+    if(!this.screenNumber) {
+        return;
+    }
+
+    this.parseButtonString();
+
+    this.buttonPressed = 3;
+    this.numberPressed = 3;
+};
+
 angular.module('myApp.memory', ['ngRoute', 'ui.bootstrap'])
 
     .config(['$routeProvider', function ($routeProvider) {
@@ -10,5 +51,24 @@ angular.module('myApp.memory', ['ngRoute', 'ui.bootstrap'])
     .controller('MemoryController', ['$scope', '$route', function MemoryController($scope, $route) {
         $scope.reloadRoute = function () {
             $route.reload();
+        };
+
+        $scope.validScreenNumberRegex = "[1234]"
+
+        $scope.stages = [
+            new stage(1),
+            new stage(2),
+            new stage(3),
+            new stage(4),
+            new stage(5)
+        ];
+
+        stage.prototype.showPosition = function() {
+            if(this.stageNumber == 1) {
+                return true;
+            }
+
+            // get number pressed of previous stage (stage numbers are 1 indexed)
+            return $scope.stages[(this.stageNumber - 1) - 1].numberPressed != null;
         };
     }]);
