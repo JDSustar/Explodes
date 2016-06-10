19699f330c48bbdace9b1b688f66d612cb187ea6
Chris Hofer <HoferC@users.noreply.github.com>
2016-06-09 23:04:20 -0400

    Start at a fix for showing instructions too soon.

--- app/complicatedWires/complicatedWires.js
+++ app/complicatedWires/complicatedWires.js
@@ -38,8 +38,8 @@ angular.module('myApp.complicatedWires', ['ngRoute', 'ui.bootstrap'])
 
         $scope.instructions = "Red:Cut Green:Don't Cut";
         $scope.serial = "";
-        $scope.batteries = null;
-        $scope.parallelPort = false;
+        $scope.batteries = "Unknown";
+        $scope.parallelPort = null;
         $scope.wires = [
             new Wire("1"),
             new Wire("2"),
@@ -50,8 +50,14 @@ angular.module('myApp.complicatedWires', ['ngRoute', 'ui.bootstrap'])
         ];
         $scope.updateResults = function () {
             console.log("Updating Results!");
+            var serialUnknown = $scope.serial !== "";
             var serialCheck = isLastDigitEven($scope.serial);
+            var batteriesUnknown = $scope.batteries === "Unknown";
             var twoOrMoreBatteries = $scope.batteries === "2" || $scope.batteries === "3+";
+            var parallelUnknown = $scope.parallelPort === null;
+            if (parallelUnknown) {
+                console.log("Parallel port unknown");
+            }
             var hasParallelPort = $scope.parallelPort === true;
             // Venn Diagram magic
             for (var i = 0; i < $scope.wires.length; i++)
@@ -73,21 +79,42 @@ angular.module('myApp.complicatedWires', ['ngRoute', 'ui.bootstrap'])
                 }
                 if (((wire.red || wire.blue) && (!wire.star && !wire.led)) || (wire.red && wire.blue && !wire.star && wire.led))
                 {
+                    if (serialUnknown)
+                    {
+                        // Unknown
+                        wire.valid = false;
+                        continue;
+                    }
                     wire.cut = serialCheck;
                     continue;
                 }
                 if (wire.blue && ((!(wire.red && wire.star) && wire.led) || (!wire.blue && wire.star && !wire.led) || (wire.blue && !wire.star && wire.led)))
                 {
+                    if (parallelUnknown)
+                    {
+                        // Unknown
+                        $scope.instructions = "Does the bomb have a parallel port?";
+                        wire.valid = false;
+                        continue;
+                    }
                     wire.cut = hasParallelPort;
                     continue;
                 }
                 if ((!wire.blue && wire.led) && ((wire.red && !wire.star) || (wire.red && wire.star) || (!wire.red && wire.star)))
                 {
+                    if (batteriesUnknown)
+                    {
+                        // Unknown
+                        $scope.instructions = "Please enter the number of batteries!";
+                        wire.valid = false;
+                        continue;
+                    }
                     wire.cut = twoOrMoreBatteries;
                     continue;
                 }
                 wire.cut = false;
             }
+
         };
         $scope.updateResults();
     }]);
