40c62b3aebf202426936da58721122ba5db08646
Chris Hofer <HoferC@users.noreply.github.com>
2016-06-16 20:45:35 -0400

    Fix for instructions showing too soon -- specific questions such as number of batteries or parallel port will appear on the right side panel. Fixed a bug where serial number was not being required when it should have been.

--- app/complicatedWires/complicatedWires.js
+++ app/complicatedWires/complicatedWires.js
@@ -48,17 +48,16 @@ angular.module('myApp.complicatedWires', ['ngRoute', 'ui.bootstrap'])
             new Wire("5"),
             new Wire("6")
         ];
+        $scope.warningInstructions = false;
         $scope.updateResults = function () {
-            console.log("Updating Results!");
-            var serialUnknown = $scope.serial !== "";
+            var serialUnknown = $scope.serial === "";
             var serialCheck = isLastDigitEven($scope.serial);
             var batteriesUnknown = $scope.batteries === "Unknown";
             var twoOrMoreBatteries = $scope.batteries === "2" || $scope.batteries === "3+";
             var parallelUnknown = $scope.parallelPort === null;
-            if (parallelUnknown) {
-                console.log("Parallel port unknown");
-            }
+            var instructionsChanged = false;
             var hasParallelPort = $scope.parallelPort === true;
+
             // Venn Diagram magic
             for (var i = 0; i < $scope.wires.length; i++)
             {
@@ -75,6 +74,7 @@ angular.module('myApp.complicatedWires', ['ngRoute', 'ui.bootstrap'])
                 if (!wire.red && ((!wire.blue && !wire.star && !wire.led) || (!wire.blue && wire.star && !wire.led) || (wire.blue && !wire.star && wire.led)))
                 {
                     wire.cut = true;
+                    //console.log("Rule 1 Success");
                     continue;
                 }
                 if (((wire.red || wire.blue) && (!wire.star && !wire.led)) || (wire.red && wire.blue && !wire.star && wire.led))
@@ -82,10 +82,13 @@ angular.module('myApp.complicatedWires', ['ngRoute', 'ui.bootstrap'])
                     if (serialUnknown)
                     {
                         // Unknown
+                        $scope.instructions = "Please enter at least the last digit of the serial number!";
+                        instructionsChanged = true;
                         wire.valid = false;
                         continue;
                     }
                     wire.cut = serialCheck;
+                    //console.log("Rule 2 - Serial Dependent");
                     continue;
                 }
                 if (wire.blue && ((!(wire.red && wire.star) && wire.led) || (!wire.blue && wire.star && !wire.led) || (wire.blue && !wire.star && wire.led)))
@@ -94,10 +97,12 @@ angular.module('myApp.complicatedWires', ['ngRoute', 'ui.bootstrap'])
                     {
                         // Unknown
                         $scope.instructions = "Does the bomb have a parallel port?";
+                        instructionsChanged = true;
                         wire.valid = false;
                         continue;
                     }
                     wire.cut = hasParallelPort;
+                    //console.log("Rule 3 - Parallel Dependent");
                     continue;
                 }
                 if ((!wire.blue && wire.led) && ((wire.red && !wire.star) || (wire.red && wire.star) || (!wire.red && wire.star)))
@@ -106,15 +111,25 @@ angular.module('myApp.complicatedWires', ['ngRoute', 'ui.bootstrap'])
                     {
                         // Unknown
                         $scope.instructions = "Please enter the number of batteries!";
+                        instructionsChanged = true;
                         wire.valid = false;
                         continue;
                     }
                     wire.cut = twoOrMoreBatteries;
+                    //console.log("Rule 4 - Battery dependent");
                     continue;
                 }
                 wire.cut = false;
             }
-
+            if (!instructionsChanged)
+            {
+                $scope.warningInstructions = false;
+                $scope.instructions = "Red = Cut, Green = Don't Cut";
+            }
+            else
+            {
+                $scope.warningInstructions = true;
+            }
         };
         $scope.updateResults();
     }]);
